a1241a3989cd95db9e6d16d2943fd698d4e93311
sbmd <sbmd@ukr.net>
2017-11-12 13:25:40 +0200

    config 2

--- common/config/main.php
+++ common/config/main.php
@@ -4,17 +4,19 @@ return [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
-    'db' => [
-        'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=timetobu.mysql.tools;dbname=timetobu_leather',
-        'username' => 'timetobu_leather',
-        'password' => '6nkjjebc',
-        'charset' => 'utf8',
-    ],
+    
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
+        ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=timetobu.mysql.tools;dbname=timetobu_leather',
+            'username' => 'timetobu_leather',
+            'password' => '6nkjjebc',
+            'charset' => 'utf8',
         ],
     ],
 ];
