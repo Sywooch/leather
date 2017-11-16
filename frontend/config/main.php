21b2200e3a9f7cf16fe92cabced54fea74103b28
sbmd <sbmd@ukr.net>
2017-11-16 21:57:28 +0200

    fix

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -61,7 +61,7 @@ return [
             'datetimeFormat' => 'd/M/Y H:i',
             'decimalSeparator' => ',',
             'thousandSeparator' => ' ',
-            'currencyCode' => '$',
+            'currencyCode' => '',
         ],
         
     ],
