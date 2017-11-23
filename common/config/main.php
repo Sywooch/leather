094a1bceb919e094679500d7473b5246616d4979
sbmd <sbmd@ukr.net>
2017-11-23 18:16:54 +0200

    email

--- common/config/main.php
+++ common/config/main.php
@@ -11,22 +11,22 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => false,
-            'transport' => [
-                'class' => 'Swift_SmtpTransport',
-                'host' => 'mail.ukraine.com.ua',
-                'username' => 'info@diano.store',
-                'password' => 'RR392EIesui4',
-                'port' => '2525',
-                'encryption' => 'tls',
-            ],
-        ],
+        // 'mailer' => [
+        //     'class' => 'yii\swiftmailer\Mailer',
+        //     'viewPath' => '@common/mail',
+        //     // send all mails to a file by default. You have to set
+        //     // 'useFileTransport' to false and configure a transport
+        //     // for the mailer to send real emails.
+        //     'useFileTransport' => false,
+        //     'transport' => [
+        //         'class' => 'Swift_SmtpTransport',
+        //         'host' => 'mail.ukraine.com.ua',
+        //         'username' => 'info@diano.store',
+        //         'password' => 'RR392EIesui4',
+        //         'port' => '2525',
+        //         'encryption' => 'tls',
+        //     ],
+        // ],
     ],
     'modules' => [
        'treemanager' =>  [
